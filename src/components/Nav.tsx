66c4df7e6e571ced55872452aa2e1aca4f1af7e2
Adam Sisk <adamsisk91@gmail.com>
2023-04-30 21:11:51 -0500

    fixes nav logo spacing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,12 @@ import siteLogo from '../assets/site-logo.svg';
 function Nav() {
   return (
     <nav className='h-14 sm:h-24 flex items-center'>
-      <img src={siteLogo} className='sm:h-14' height='56' alt='A S site logo' />
+      <img
+        src={siteLogo}
+        className='h-10 sm:h-14'
+        height='56'
+        alt='A S site logo'
+      />
     </nav>
   );
 }
