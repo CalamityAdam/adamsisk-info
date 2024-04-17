331c2fd3da8bd04ce083a194019fbb39c934a280
Adam Sisk <adamsisk91@gmail.com>
2024-04-17 10:56:51 -0500

    fixe mobile responsiveness in markdown wrapper

--- src/components/MarkdownWrapper.tsx
+++ src/components/MarkdownWrapper.tsx
@@ -43,7 +43,7 @@ function MarkdownWrapper({
   };
 
   return (
-    <div className='prose lg:prose-lg mx-auto leading-6'>
+    <div className='overflow-auto prose lg:prose-lg mx-auto leading-6'>
       <ReactMarkdown
         remarkPlugins={[remarkGfm, breaks]}
         children={children}
