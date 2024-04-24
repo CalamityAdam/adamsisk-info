175f960f946c99ae9f6e1b0a5e22dcd33d5f527e
Adam Sisk <adamsisk91@gmail.com>
2024-04-23 21:45:43 -0500

    fixup

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,6 +1,8 @@
 function Body({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex-1 flex items-center justify-center'>{children}</div>
+    <div className='flex-1 flex w-full items-center justify-center'>
+      {children}
+    </div>
   );
 }
 
